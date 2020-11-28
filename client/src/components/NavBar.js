19942a126cf3aca21a3a0e67c90c4f3ed879fa61
WillV1 <wmvanhook@gmail.com>
2020-11-28 16:13:35 -0500

    Add logout and authenticate navlinks

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,49 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../actions/auth';
 import M from "materialize-css";
 
-const NavBar = () => {
+const NavBar = ({ auth: { isAuthenticated, loading}, logout}) => {
+
+  const authLinks = (
+    <Fragment>
+    <ul id="nav-mobile" className="right hide-on-med-and-down">
+    <li><Link to="/new">Add Recipient</Link></li>
+    <li><Link to="/budget">Check Budget</Link></li>
+    <li><Link to="/main">Home</Link></li>
+    <li><Link onClick={logout} to="/">Log Out</Link></li>
+  </ul>
+
+  <ul id="slide-out" className="sidenav">
+        <li><Link to="/new">Add Recipient</Link></li>
+        <li><Link to="/budget">Check Budget</Link></li>
+        <li><Link to="/main">Home</Link></li>
+        <li><Link onClick={logout} to="/">Log Out</Link></li>
+      </ul>
+      <a href="/" data-target="slide-out" className="sidenav-trigger">
+      <i className="material-icons">menu</i></a>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+    <ul id="nav-mobile" className="right hide-on-med-and-down">
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/signup">Sign Up</Link></li>
+    <li><Link to="/login">Log In</Link></li>
+  </ul>
+
+  <ul id="slide-out" className="sidenav">
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/signup">Sign Up</Link></li>
+      <li><Link to="/login">Log In</Link></li>
+    </ul>
+    <a href="/" data-target="slide-out" className="sidenav-trigger">
+      <i className="material-icons">menu</i></a>
+    </Fragment>
+  );
 
   useEffect(() => {
     let sidenav = document.querySelector('#slide-out');
@@ -13,32 +54,21 @@ const NavBar = () => {
     <div>
       <nav>
       <div className="nav-wrapper">
-        <a href="/" className="brand-logo">Gift Tracker</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><Link to="/new">Add Recipient</Link></li>
-          <li><Link to="/budget">Check Budget</Link></li>
-          <li><Link to="/main">Home</Link></li>
-          <li><Link to="/logout">Log Out</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/login">Log In</Link></li>
-        </ul>
-      </div>
+        <Link to="/" className="brand-logo">Gift Tracker</Link>
+        {!loading && <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>}
+      </div>     
       </nav>
-
-      <ul id="slide-out" className="sidenav">
-        <li><Link to="/new">Add Recipient</Link></li>
-        <li><Link to="/budget">Check Budget</Link></li>
-        <li><Link to="/main">Home</Link></li>
-        <li><Link to="/logout">Log Out</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/signup">Sign Up</Link></li>
-        <li><Link to="/login">Log In</Link></li>
-      </ul>
-      <a href="/" data-target="slide-out" className="sidenav-trigger">
-      <i className="material-icons">menu</i></a>
     </div>
   )
 };
 
-export default NavBar;
+NavBar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(NavBar);
