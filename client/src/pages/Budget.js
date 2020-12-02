85981146450de258ae904ee2bebcbf6316c4936b
WillV1 <wmvanhook@gmail.com>
2020-12-01 23:29:38 -0500

    Start budget page

--- client/src/pages/Budget.js
+++ client/src/pages/Budget.js
@@ -1,9 +1,46 @@
-const Budget = () => {
-  return (
-    <div>
-      <h1>Budget</h1>
-    </div>
-  )
-}
+import {Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getRecipients } from '../actions/recipient';
+import Spinner from '../layout/Spinner';
+import { PieChart } from 'react-minimal-pie-chart';
 
-export default Budget;
+
+const Main = ({ getRecipients, recipient: { recipients, loading} }) => {
+
+  useEffect(() => {
+    getRecipients();
+  }, [getRecipients])
+
+  {recipients.map(recipient => (
+    <RecipientItem key={recipient._id} recipient={recipient}/>
+    ))}
+
+  return loading ? <Spinner /> : <Fragment>
+  <div className="container">
+    <div className="row">
+      <div className="col s3">
+        <h2>Budget to Actual</h2>
+      </div>
+
+      <PieChart data={[
+        {title: 'Gift Budget'}
+        {title: 'Spent to Date'}
+      ]}
+      />
+    </div>
+  </div>
+  
+  </Fragment>
+};
+
+Main.propTypes = {
+  getRecipients: PropTypes.func.isRequired,
+  recipient: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  recipient: state.recipient
+})
+
+export default connect(mapStateToProps, {getRecipients})(Main);
