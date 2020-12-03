12c1572b3a0c661f2e9404366d8352b3b20864ef
WillV1 <wmvanhook@gmail.com>
2020-12-03 07:53:56 -0500

    Finish about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,16 +2,16 @@ const About = () => {
   return (
     <div>
       <h1>About Gift Tracker</h1>
-      <p>Tired of boring spreadsheets?  Look no further than Gift Tracker.</p>
+      <h6>Tired of boring spreadsheets?  Look no further than Gift Tracker.</h6>
       <hr />
-      <p>Providing an easy sign-up and log-in, you have a one stop shop to view the names of
-      those special people in your life for whom you want to make some purchases.</p>
-      <p>Easily add and view those special people's profiles, add and track gifts to make sure you
-      are spending how much you want on each person throughout the process.</p>
-      <p>Finally, to make sure you are spending mindfully, check out our budget dashboard to see
-      overall budget and spending vs. remaining budget.</p>
+      <h6>Providing an easy sign-up and log-in, you have a one stop shop to view the names of
+      those special people in your life for whom you want to make some purchases.</h6>
+      <h6>Easily add and view those special people's profiles, add and track gifts to make sure you
+      are spending how much you want on each person throughout the process.</h6>
+      <h6>Finally, to make sure you are spending mindfully, check out our budget dashboard to see
+      overall budget and spending vs. remaining budget.</h6>
       <hr />
-      <p> With everything you need one click away, why not give Gift Tracker your time?</p>
+      <h6> With everything you need one click away, why not give Gift Tracker your time?</h6>
     </div>
   )
 }
