19fdc637f40720016a4bcfdfe3920a47379547bf
WillV1 <wmvanhook@gmail.com>
2020-12-03 22:34:31 -0500

    Update package.json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const auth = require('./middleware/auth');
+const path = require('path');
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3001;
@@ -25,5 +26,15 @@ app.use('/users', ctrl.users);
 app.use('/profile', ctrl.profile),
 app.use('/recipients', ctrl.recipients);
 app.use('/auth', routes.auth);
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
